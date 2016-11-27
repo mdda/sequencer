56fee445d4854761a40540a21408d8955f3eb828
Martin Andrews <mdda@redcatlabs.com>
2016-11-27 11:46:36 +0800

    Better modularization

--- javascripts/ev.js
+++ javascripts/ev.js
@@ -1,28 +1,41 @@
 var ev = {
+  audioContext:undefined,
+  
   init: (audioContext) => {
-      console.log("ev loaded");
+      console.log("ev loading");
+      ev.audioContext=audioContext;
       
       function onError() {  }
       
-      var get_sampleAsync = (f => {
-          return new Promise( (resolve, reject) => {
-              var request = new XMLHttpRequest();
-              request.open('GET', "./audio/"+f, true);
-              request.responseType = 'arraybuffer';
-              request.onload = _ => {
-                  audioContext.decodeAudioData(request.response, arrayBuffer => {
-                      resolve(arrayBuffer);
-                    }, e => {
-                      console.log("Failed to download "+f);
-                      reject();
-                    });
-                };
-              request.send();
-            });
-        });
- 
-      get_sampleAsync('synthlead.wav').then( buffer => {
+      var cloudBtn = document.getElementById("evBtn1");
+      cloudBtn.addEventListener('click', function(time) {
+          ev.cloud();
+        }, false);
+        
+      ev.get_sampleAsync('synthlead.wav').then( buffer => {
           console.log("Got buffer");
         });
-    }  
+
+    },
+    
+  get_sampleAsync: f => {
+      return new Promise( (resolve, reject) => {
+          var request = new XMLHttpRequest();
+          request.open('GET', "./audio/"+f, true);
+          request.responseType = 'arraybuffer';
+          request.onload = _ => {
+              ev.audioContext.decodeAudioData(request.response, arrayBuffer => {
+                  resolve(arrayBuffer);
+                }, e => {
+                  console.log("Failed to download "+f);
+                  reject();
+                });
+            };
+          request.send();
+        });
+    },
+    
+  cloud: _=>{
+      console.log("Cloud!");
+    }
 };
